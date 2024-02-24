abca8ecb932327a3fa59f48b1b72d6dc5c956540
JargeZ <lev@ph3.ru>
2024-02-24 16:06:47 +1100

    upd

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void SetAnimation(string animation)
+    {
+        _animator.Play(animation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GameLogicScript.cs
+++ Assets/Scripts/GameLogicScript.cs
@@ -2,28 +2,93 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+public enum GameResult
+{
+    Draw,
+    Player1Wins,
+    Player2Wins
+}
+
 public class GameLogicScript : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text _result;
+    public CharController _character;
 
     public string ChooseLeft { get; set; }
     public string ChooseRight { get; set; }
 
+    private void ResetGame()
+    {
+        ChooseLeft = "";
+        ChooseRight = "";
+    }
+
+    private string getScreenText(GameResult result)
+    {
+        switch (result)
+        {
+            case GameResult.Draw:
+                return "Draw!";
+            case GameResult.Player1Wins:
+                return "Player 1 wins!";
+            case GameResult.Player2Wins:
+                return "Player 2 wins!";
+            default:
+                throw new System.Exception("Unknown game result");
+        }
+    }
+
+    private void setResultAnimation(GameResult result)
+    {
+        switch (result)
+        {
+            case GameResult.Draw:
+                _character.SetAnimation("Draw");
+                break;
+            case GameResult.Player1Wins:
+                _character.SetAnimation("Win");
+                break;
+            case GameResult.Player2Wins:
+                _character.SetAnimation("Lose");
+                break;
+            default:
+                throw new System.Exception("Unknown game result");
+        }
+    }
+
     // Этот метод вызывается, например, при нажатии кнопки сравнения
     public void CompareSharedValues()
     {
-        _result.text = DetermineWinner(ChooseLeft, ChooseRight);
-        Debug.Log("update " + ChooseLeft + " / " + ChooseRight + " = " + _result.text);
+
+        if ((ChooseLeft != "" && ChooseRight == "")||(ChooseLeft == "" && ChooseRight != ""))
+        {
+            _character.SetAnimation("Idle");
+            _result.text = "Game starts!";
+        }
+
+        if (ChooseLeft != "" && ChooseRight != "")
+        {
+            GameResult result = DetermineWinner(ChooseLeft, ChooseRight);
+            _result.text = getScreenText(result);
+            setResultAnimation(result);
+            ResetGame();
+        }
+    }
+
+    void Start()
+    {
+        ResetGame();
+        _character = CharController.FindFirstObjectByType<CharController>();
     }
     
-    public string DetermineWinner(string value1, string value2)
+    public GameResult DetermineWinner(string value1, string value2)
     {
 
         // Правила камень-ножницы-бумаги
         if (value1 == value2)
         {
-            return "It's a draw!";
+            return GameResult.Draw;
         }
         else if (
             (value1 == "Rock" && value2 == "Scissors") ||
@@ -31,11 +96,11 @@ public class GameLogicScript : MonoBehaviour
             (value1 == "Scissors" && value2 == "Paper")
         )
         {
-            return "Player 1 wins!";
+            return GameResult.Player1Wins;
         }
         else
         {
-            return "Player 2 wins!";
+            return GameResult.Player2Wins;
         }
     }
 }
