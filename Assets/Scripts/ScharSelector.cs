e227da13c901d8b399d2a8f299e595ff9ad35c11
JargeZ <lev@ph3.ru>
2024-04-03 20:47:19 +1100

    Char select

--- Assets/Scripts/ScharSelector.cs
+++ Assets/Scripts/ScharSelector.cs
@@ -3,6 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class SelectedPlayerInfo
+{
+    public string SelectedPlayerName;
+    public string SelectedCharSkin;
+}
+
+public static class SelectScreen
+{
+    public static SelectedPlayerInfo LeftPlayerInfo = new SelectedPlayerInfo();
+    public static SelectedPlayerInfo RightPlayerInfo = new SelectedPlayerInfo();
+}
+
+
+public enum PlayerSide
+{
+    Left,
+    Right
+}
 
 public class ScharSelector : MonoBehaviour
 {
@@ -10,7 +32,7 @@ public class ScharSelector : MonoBehaviour
     public SpriteRenderer SpriteRenderer;
     public List<Sprite> skins = new List<Sprite>();
     private int selectedSkin = 0;
-    public GameObject playerskin;
+    public PlayerSide playerSide;
 
    public void NextOption()
     {
@@ -34,7 +56,32 @@ public class ScharSelector : MonoBehaviour
 
     public void PlayGame()
     {
+        Dictionary<int, string> indexToCharNameMapping = new Dictionary<int, string>
+        {
+            {0, "nokia"},
+            {1, "pavel"},
+            {2, "dean"},
+            {3, "vanish"},
+        };
+
+        string currentCharSkinName = indexToCharNameMapping[selectedSkin];
+        string currentPlayerName = transform.parent.GetComponentsInChildren<TMP_Text>()[0].text;
+        Debug.Log("Selected character: " + currentCharSkinName);
+        Debug.Log("Selected player name: " + currentPlayerName);
+        
+        if (playerSide == PlayerSide.Left)
+        {
+            SelectScreen.LeftPlayerInfo.SelectedCharSkin = currentCharSkinName;
+            SelectScreen.LeftPlayerInfo.SelectedPlayerName = currentPlayerName;
+        }
+        if (playerSide == PlayerSide.Right)
+        {
+            SelectScreen.RightPlayerInfo.SelectedCharSkin = currentCharSkinName;
+            SelectScreen.RightPlayerInfo.SelectedPlayerName = currentPlayerName;
+        }
+
+
         SceneManager.LoadScene("SampleScene");
-            }
+    }
 
 }
